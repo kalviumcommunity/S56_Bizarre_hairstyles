b7161efa00edd5aa7254f0052128b677dd73c271
CHINMAYEEHARANE58 <chinmayee.harane@kalvium.community>
2024-02-23 15:58:11 +0530

    assignment mongoDB client completed

---
+++ config/db.js
@@ -0,0 +1,5 @@
+const config = {
+    mongoURI: 'mongodb+srv://chinmayeeharane:Mongodbuser@cluster0.xwpsezr.mongodb.net/bizarreHairstyles?retryWrites=true&w=majority&appName=Cluster0',
+  };
+  
+  module.exports = config;
--- server.js
+++ server.js
@@ -6,6 +6,34 @@ app.get('/ping' , (req, res) =>{
     res.status(200).send("pong")
 })
 
+const mongoose = require('mongoose');
+const mongoServer = require('./config.js')
+
+const connectToDB = async () => {
+  try {
+    await mongoose.connect(mongoServer.mongoURI)
+    console.log('connected to mongoDB');
+  } catch (err) {
+    console.error('error connecting to mongoDB:', err.message);
+  }
+};
+
+const disconnectFromDB = async () => {
+  try {
+    await mongoose.connection.close()
+    console.log('disconnected from mongoDB');
+  } catch (err) {
+    console.error('error disconnecting from mongoDB:', err.message);
+  }
+};
+
+module.exports = {
+  connectToDB,
+  disconnectFromDB,
+  mongooseConnection: mongoose.connection,
+};
+
+
 if (require.main === module) {
     app.listen(3000, (err) => {
         if (err) console.error(err);
