13051af72752e250064fa50780673cd63b140ad8
CHINMAYEEHARANE58 <chinmayee.harane@kalvium.community>
2024-02-26 15:21:17 +0530

    enhancments made

--- routes.js
+++ routes.js
@@ -1,22 +1,38 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/get',(req,res)=>{
-    res.send("Its a get req")
+
+router.get('/get', async (req, res, next) => {
+    try {
+        res.send({ message: "GET request processed successfully." })
+    } catch (error) {
+        next(error);
+    }
 })
 
-router.post('/post',(req,res)=>{
-    res.send("Its a post req")
+router.post('/post', async (req, res, next) => {
+    try {
+        res.send({ message: "POST request processed successfully." })
+    } catch (error) {
+        next(error);
+    }
 })
 
-router.patch('/patch',(req,res)=>{
-    res.send("Its a patch req")
+router.patch('/patch', async (req, res, next) => {
+    try {
+        res.send({ message: "Patch request processed successfully." })
+    } catch (error) {
+        next(error);
+    }
 })
 
-router.delete('/delete',(req,res)=>{
-    res.send("Its a delete req")
+router.delete('/delete', async (req, res, next) => {
+    try {
+        res.send({ message: "Delete request processed successfully." })
+    } catch (error) {
+        next(error);
+    }
 })
-
 
 module.exports = {router}
 
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const {router} = require('./routes.js')
 const app = express();
-// app.use(express.json());
+app.use(express.json());
 
 app.get('/ping' , (req, res) =>{
     res.status(200).send("pong")
