44fb2f60a82b183ed350b5d7e2b7a1068c69a6ca
CHINMAYEEHARANE58 <chinmayee.harane@kalvium.community>
2024-03-01 11:29:33 +0530

    dummy datacompleted

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import './App.css'
 import Landing from './Components/Landing.jsx'
+import Explore from './Components/Explore.jsx'
 
 function App() {
 
   return (
     <>
       <Landing/>
+      <Explore/>
     </>
   )
 }
