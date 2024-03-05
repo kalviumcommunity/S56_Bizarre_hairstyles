f789124463f07dac0d423273b59e06da8adbeeb6
CHINMAYEEHARANE58 <chinmayee.harane@kalvium.community>
2024-03-05 10:40:47 +0530

    Data fetched from mongoDB to server

--- Frontend/src/Components/Explore.jsx
+++ Frontend/src/Components/Explore.jsx
@@ -3,8 +3,20 @@ import './Explore.css'
 import explore from "../assets/exploreImg.jpg"
 
 export default function Explore() {
+
+    const [data, setData] = useState([])
+    useEffect(() => {
+      axios
+        .get('')
+        .then((res) => {
+          console.log(res.data)
+          setData(res.data)
+        })
+    }, [])
+
   return (
     <>
+      
       <h1 className='explore-title'>Explore</h1>
       <div className='explore'>
         <img className='explore-img' src={explore} alt="Explore Img" />
@@ -18,17 +30,23 @@ export default function Explore() {
         </div>        
       </div>
 
-      {/* <div className='explore'>
-        <img className='explore-img' src={explore} alt="Explore Img" />
-        <div className='details'>
-          <h3 className='title'>Glitter Beard</h3>
-          <h4 className='description-img'>Beard covered in glitter</h4>
-          <h4 className='bizarreness'>Bizarreness level: 5</h4>
-          <h5 className='category'>Category: Hipster</h5>
-          <h5 className='accessories'>Accessories: Glitter gel, stencil </h5>
-          <button className='explore-button'>Explore</button>
-        </div>        
-      </div> */}
+      
+
+        {/* {data && data.map((info) => {
+					return (
+						<div className='explore'>
+              <img className='explore-img' src={explore} alt="Explore Img" />
+              <div className='details'>
+                <h3 className='title'>Glitter Beard</h3>
+                <h4 className='description-img'>Beard covered in glitter</h4>
+                <h4 className='bizarreness'>Bizarreness level: 5</h4>
+                <h5 className='category'>Category: Hipster</h5>
+                <h5 className='accessories'>Accessories: Glitter gel, stencil </h5>
+                <button className='explore-button'>Explore</button>
+              </div>        
+            </div>
+					)
+				})} */}
     </>
   )
 }
